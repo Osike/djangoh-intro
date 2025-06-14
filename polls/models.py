024ff01f1f1873d45610177bacd98dcfe9031b78
Osike <shadyabacus@gmail.com>
2025-06-14 14:05:44 +0300

    huh

--- polls/models.py
+++ polls/models.py
@@ -11,7 +11,7 @@ class Question(models.Model):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField("date published")
+    pub_date = models.DateTimeField("date publisd")
 
 
 class Choice(models.Model):
